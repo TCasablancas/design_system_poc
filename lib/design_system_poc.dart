af4c859dade9666fd9f78c8b0e2bbd29c4469ea7
Thiago Augusto Da Costa Silva <thyago_casablancas@hotmail.com>
2022-03-01 20:19:15 -0300

    add app dimensions

--- lib/design_system_poc.dart
+++ lib/design_system_poc.dart
@@ -2,5 +2,7 @@ library design_system_poc;
 
 export 'src/shared/app_colors.dart';
 export 'src/shared/ui_helpers.dart';
+export 'src/shared/app_dimensions.dart';
+
 export 'src/widgets/main_button.dart';
 export 'src/widgets/main_card.dart';
