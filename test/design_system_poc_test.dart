8da841bc4c9c5b7b42064328c03a6d7e34fa51e4
Thiago Augusto Da Costa Silva <thyago_casablancas@hotmail.com>
2022-03-01 20:29:11 -0300

    Change on test

--- test/design_system_poc_test.dart
+++ test/design_system_poc_test.dart
@@ -4,8 +4,8 @@ import 'package:design_system_poc/design_system_poc.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
+    final card = MainCard();
+    expect(card.build(context));
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
   });
