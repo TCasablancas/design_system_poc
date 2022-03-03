6dec4822cee9f0dc346d39325e2c1459ead1fe9d
Thiago Augusto Da Costa Silva <thyago_casablancas@hotmail.com>
2022-03-03 16:25:54 -0300

    Update box

--- lib/src/widgets/main_card.dart
+++ lib/src/widgets/main_card.dart
@@ -1,27 +1,42 @@
 import 'package:design_system_poc/src/shared/app_colors.dart';
 import 'package:design_system_poc/src/shared/app_dimensions.dart';
+import 'package:design_system_poc/src/shared/font_styles.dart';
 import 'package:design_system_poc/src/shared/ui_helpers.dart';
 import 'package:flutter/src/painting/border_radius.dart';
 import 'package:flutter/material.dart';
 
 class MainCard extends StatelessWidget {
   final String boxTitle;
+  final Color background;
 
-  const MainCard({required this.boxTitle});
+  const MainCard({
+    required this.boxTitle,
+    required this.background,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Container(
-          width: MediaQuery.of(context).size.width - 32,
-          height: 120,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: Colors.white,
-              boxShadow: [BoxShadow(color: Colors.blueGrey, spreadRadius: 4)]),
-          child: Text(boxTitle),
-        ),
+            width: MediaQuery.of(context).size.width - 32,
+            height: 120,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+                color: mainPrimaryColor,
+                boxShadow: [
+                  BoxShadow(color: Colors.blueGrey, spreadRadius: 4)
+                ]),
+            child: Padding(
+                padding: EdgeInsets.symmetric(vertical: 12, horizontal: 12),
+                child: Center(
+                    child: Text(
+                  boxTitle,
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w400,
+                      color: Colors.white),
+                )))),
       ),
     );
   }
