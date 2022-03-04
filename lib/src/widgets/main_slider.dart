eeef6b905e4fdb331494714eb211353820afd8d4
Thiago Augusto Da Costa Silva <thyago_casablancas@hotmail.com>
2022-03-04 09:42:30 -0300

    setting separator

--- lib/src/widgets/main_slider.dart
+++ lib/src/widgets/main_slider.dart
@@ -7,9 +7,10 @@ class MainSlider extends StatelessWidget {
   const MainSlider({required this.cards});
 
   @override
-  Widget build(BuildContext context) => ListView.builder(
+  Widget build(BuildContext context) => ListView.separated(
       scrollDirection: Axis.horizontal,
       itemCount: 5,
+      separatorBuilder: (context, _) => SizedBox(width: 15),
       padding: EdgeInsets.symmetric(vertical: 16, horizontal: 16),
       itemBuilder: (BuildContext context, int index) => Row(children: [cards]));
 }
